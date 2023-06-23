24680e9c0d4177d1f2e478befa7988579fc02f31
Carl Saginsin <saguinsincarl8@gmail.com>
2023-06-23 20:08:48 +0700

    23-06-2023 Updates
    I added a new cards, Created a Jquery functions.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
-    $('.nav-link').hover(
-      function() {
-        $(this).addClass('hovered');
-      },
-      function() {
-        $(this).removeClass('hovered');
-      }
-    );
+  $('.nav-link').hover(
+    function() {
+      $(this).addClass('hovered');
+    },
+    function() {
+      $(this).removeClass('hovered');
+    }
+  );
+});
+
+$(document).ready(function() {
+  $('#myButton').click(function() {
+    window.location.href = 'homeservice.html';
   });
+});
+
+$(document).ready(function() {
+  $('#mysecondButton').click(function() {
+    window.location.href = 'rebond.html';
+  });
+});
+
+$(document).ready(function() {
+  $('#mythirdButton').click(function() {
+    window.location.href = 'haircut.html';
+  });
+});
+
