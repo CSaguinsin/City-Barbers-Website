84df051590704a78f0ced80563ffa06879464b72
Carl Saginsin <saguinsincarl8@gmail.com>
2023-06-24 20:50:33 +0700

    24-06-2023 I added a footer

--- js/script.js
+++ js/script.js
@@ -28,3 +28,29 @@ $(document).ready(function() {
 });
 
 
+   // Text to be displayed with typing effect
+   const text = "WHERE TO FIND US?";
+
+   // Delay between each character (in milliseconds)
+   const typingSpeed = 100;
+ 
+   // Get the text container element
+   const textContainer = document.querySelector(".secondheader");
+ 
+   // Function to display text with typing effect
+   function typeText() {
+     let index = 0;
+     const intervalId = setInterval(() => {
+       textContainer.textContent += text[index];
+       index++;
+ 
+       if (index === text.length) {
+         clearInterval(intervalId);
+         setTimeout(() => {
+           textContainer.textContent = ""; // Clear the text
+           typeText(); // Call the function again to start the loop
+         }, 2000); // Delay before starting the loop again (in milliseconds)
+       }
+     }, typingSpeed);
+   }
+ 
